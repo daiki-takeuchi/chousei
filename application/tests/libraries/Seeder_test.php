23308eac4dfc48bc60f971b122a3bf64de8ccc2e
d.takeuchi <d.takeuchi@technoplan-inc.co.jp>
2016-04-09 14:59:15 +0900

    不要テストを削除

--- application/tests/libraries/Seeder_test.php
+++ application/tests/libraries/Seeder_test.php
@@ -6,8 +6,8 @@ class Seeder_test extends TestCase
     {
         $this->obj = new Seeder();
         $this->resetInstance();
-        $this->CI->load->model('News_model');
-        $this->news_model = $this->CI->News_model;
+        $this->CI->load->model('Users_model');
+        $this->users_model = $this->CI->Users_model;
     }
 
     /**
@@ -16,21 +16,7 @@ class Seeder_test extends TestCase
     public function Seederの実行()
     {
         $expected = 3;
-        $this->obj->call('NewsSeeder');
-        $this->assertEquals($expected, $this->news_model->get_count_all());
-    }
-
-    /**
-     * @test
-     */
-    public function マジックメソッドの確認()
-    {
-        $expected = 3;
-        $this->obj->load->model('Users_model');
         $this->obj->call('UsersSeeder');
-        $this->users_model = $this->CI->Users_model;
-        $sut = $this->users_model->find();
-
-        $this->assertEquals($expected, count($sut));
+        $this->assertEquals($expected, $this->users_model->get_count_all());
     }
 }
